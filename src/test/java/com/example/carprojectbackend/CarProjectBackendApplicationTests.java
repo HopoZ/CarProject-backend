2cddc0aa34438ff63647df171b8387bf12e7a296
zlx <505925146@qq.com>
2024-03-15 19:09:17 +0800

    first commit

---
+++ src/test/java/com/example/carprojectbackend/CarProjectBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.carprojectbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarProjectBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
